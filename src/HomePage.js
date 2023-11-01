bcc1ea060d0cef41eb2d2d225c00c56e547e790f
ednever <edgarneverovskij@gmail.com>
2023-11-01 14:51:40 +0200

    :pushpin: Files added

---
+++ src/HomePage.js
@@ -0,0 +1,12 @@
+import { useEffect, useState } from 'react';
+
+function HomePage(){
+    
+    return (
+        <div className="App">
+          <h1>Новый проект</h1>
+        </div>
+      );
+  };
+  
+export default HomePage;
